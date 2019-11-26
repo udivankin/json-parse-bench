53f895049b29b3cca76198b082f9f7c4ceaa87ae
udi <udi@wisebits.com>
2019-11-26 17:55:23 +0200

    more configurable test

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('./data.json', 'UTF-8');
-const quoted = JSON.stringify(JSON.parse(data));
-
+const minified = JSON.stringify(JSON.parse(data));
 const unquote = (s) => s.replace(/\\"/g,"\uFFFF").replace(/\"([^"]+)\":/g,"$1:").replace(/\uFFFF/g,"\\\"");
-const createOutput = (data, key) => `console.time('${key}');\nvar data = ${data};\nconsole.timeEnd('${key}');`
+const createOutput = (key, data) => `console.time('${key}');\nvar data = ${data};\nconsole.timeEnd('${key}');`
 
-fs.writeFileSync('build/serialize-object-literal-unquoted.js', createOutput(unquote(quoted), 'object-literal-unquoted'));
-fs.writeFileSync('build/serialize-object-literal.js', createOutput(quoted, 'object-literal'));
-fs.writeFileSync('build/serialize-json-parse.js', createOutput(`JSON.parse('${quoted}')`, 'json-parse'));
-fs.writeFileSync('build/serialize-object-literal-multiline.js', createOutput(data, 'object-literal-multiline'));
+const config = [
+  ['object-literal-unquoted', unquote(minified)],
+  ['json-parse', `JSON.parse('${minified}')`],
+  ['object-literal', minified],
+  ['object-literal-multiline', data],
+];
+
+if (!fs.existsSync('build')) fs.mkdirSync('build');
+
+config.forEach((a, id) => fs.writeFileSync(`build/${id}.js`, createOutput(...a)));
